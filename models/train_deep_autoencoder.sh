c8314fda842e9d56ca51f3333736b505311b164c
Mehdi Cherti <mehdicherti@llrgrcl02.in2p3.fr>
2015-01-30 11:11:45 +0100

    enhancing the models

--- models/train_deep_autoencoder.sh
+++ models/train_deep_autoencoder.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python /home/gridcl/mehdicherti/work/code/pylearn2/pylearn2/scripts/tutorials/rbm_autoencoder/deep_autoencoder_train.py dataset.yaml auto.yaml layer.yaml sigmoid '!import pylearn2.expr.activations.relu' 256 50 20
+python /home/gridcl/mehdicherti/work/code/pylearn2/pylearn2/scripts/tutorials/rbm_autoencoder/deep_autoencoder_train.py dataset.yaml auto.yaml layer.yaml sigmoid '!import pylearn2.expr.activations.relu' 256 50
 
